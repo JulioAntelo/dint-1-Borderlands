d6e8f761ab0af254f4ac5162b36b0c6953b96388
JulioAntelo <jantram710@g.educaand.es>
2024-01-31 19:41:18 +0100

    dia 31-1-2024 clase Din

--- app/src/main/java/com/example/dint_1_borderlands/MainActivity.kt
+++ app/src/main/java/com/example/dint_1_borderlands/MainActivity.kt
@@ -4,20 +4,16 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.dint_1_borderlands.buildsdelacomunidad.BuildsDeLaComunidad
-import com.example.dint_1_borderlands.cabecerapaginainicial.CabeceraPaginaInicial
-import com.example.dint_1_borderlands.piedeapp.PieDeApp
+import com.example.dint_1_borderlands.meniniciosesinusuarioborderlands.MenInicioSesiNUsuarioBorderlands
 import com.example.dint_1_borderlands.ui.theme.Dint1BorderlandsTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,24 +39,9 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun PaginaInicial(
-
+){ Column(modifier =
+    Modifier.background(Color.Gray)
+        .fillMaxSize()
 ){
-
-    Scaffold(
-        topBar = { CabeceraPaginaInicial(
-            Modifier
-                .size(450.dp, 95.dp)
-        ) },
-        bottomBar = { PieDeApp(
-            Modifier
-                .size(415.dp, 40.dp)
-        )}
-    ){
-        Column(
-            modifier = Modifier
-                .padding(top = 95.dp, bottom = 40.dp),
-        ){
-            BuildsDeLaComunidad()
-        }
-    }
+    MenInicioSesiNUsuarioBorderlands() }
 }
